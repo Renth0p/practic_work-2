be47ff547a4ca7bc7960310d9a9e9bdd96a91946
Renthop <44197289+Renth0p@users.noreply.github.com>
2020-10-03 18:24:57 +0300

    Update practic_file.cpp

--- practic_file.cpp
+++ practic_file.cpp
@@ -6,56 +6,201 @@ using namespace std;
 
 int main()
 {
-double x, y, s;
-int n;
+    double x, y, s, b;
+    int n;
 
-cout << "Введите значение n от 1 до 7, это число будет являться номером примера!" << endl;
-cout << "n= ";
-cin >> n;
+    cout << "Введите значение n от 1 до 7, это число будет являться номером примера" << endl;
+    cout << "n= ";
+    cin >> n;
 
-if (n == 1) {
-  cout << "Пример #1" << endl;
-  cout << "Введите x: ";
-  cin >> x;
-  cout << "Введите y: ";
-  cin >> y;
+    if (n == 1) {
 
-if (x*y > 0) {
-  s = pow((x+y), 2) - cbrt(abs(x));
-  cout << "1)s = " << setprecision(3) << s << endl;
+        cout << "Пример #1." << endl;
 
-} else if (x*y < 0) {
-  s = pow((x+y), 2) + sin(x);
-  cout << "2)s = " << setprecision(3) << s << endl;
+        cout << "Введите x: ";
+        cin >> x;
+        cout << "Введите y: ";
+        cin >> y;
 
-} else {
-  s = pow((x+y), 2) + pow(y, 3);
-  cout << "3)s = " << setprecision(3) << s << endl;
+        if (x*y > 0) {
+
+            s = pow((x+y), 2) - cbrt(abs(x));
+            cout << "1)s = " << setprecision(3) << s << endl;
+
+        } else if (x*y < 0) {
+
+            s = pow((x+y), 2) + sin(x);
+            cout << "2)s = " << setprecision(3) << s << endl;
+
+        } else {
+
+            s = pow((x+y), 2) + pow(y, 3);
+            cout << "3)s = " << setprecision(3) << s << endl;
+
+        }
+
+    } else if (n ==2) {
+
+        cout << "Пример #2." << endl;
+
+        cout << "Введите x: ";
+        cin >> x;
+        cout << "Введите y: ";
+        cin >> y;
+
+        if (x/y > 0) {
+
+            s = log(x) + cbrt(abs(y));
+            cout << "1)s = " << setprecision(3) << s << endl;
+
+        } else if (x/y < 0) {
+
+            s = log(abs(x/y))*pow((x+y), 3);
+            cout << "2)s = " << setprecision(3) << s << endl;
+
+        } else {
+
+            s = pow(pow(x, 2), 3);
+            cout << "3)s = " << setprecision(3) << s << endl;
+
+        }
+
+    } else if (n == 3) {
+
+        cout << "Пример #3." << endl;
+
+        cout << "Введите x: ";
+        cin >> x;
+        cout << "Введите y: ";
+        cin >> y;
+
+        if (x-y == 0) {
+
+            s = pow(x, 2) + cbrt(y) + sin(y);
+            cout << "1)s = " << setprecision(3) << s << endl;
+
+        } else if (x-y > 0) {
+
+            s = pow((x-y), 2) + log(abs(x));
+            cout << "2)s = " << setprecision(3) << s << endl;
+
+        } else {
+
+            s = pow((y-x), 2) + tan(y);
+            cout << "3)s = " << setprecision(3) << s << endl;
+
+        }
+
+    } else if (n == 4) {
+
+        cout << "Пример #4." << endl;
+
+        cout << "Введите x: ";
+        cin >> x;
+        cout << "Введите y: ";
+        cin >> y;
+
+        if (x > y) {
+
+            s = cbrt(abs(x-y)) + tan(x);
+            cout << "1)s = " << setprecision(3) << s << endl;
+
+        } else if (x < y) {
+
+            s = pow((y-x), 3) + cos(x);
+            cout << "2)s = " << setprecision(3) << s << endl;
+
+        } else {
+
+            s = pow((y+x), 2) + pow(x, 3);
+            cout << "3)s = " << setprecision(3) << s << endl;
+
+        }
+
+    } else if (n == 5) {
+
+        cout << "Пример #5." << endl;
+
+        cout << "Введите x: ";
+        cin >> x;
+        cout << "Введите y: ";
+        cin >> y;
+
+        if (x > y) {
+
+            s = y * sqrt(abs(x)) + 3 * sin(y);
+            cout << "1)s = " << setprecision(3) << s << endl;
+
+        } else if (x < y) {
+
+            s = x * sqrt(abs(x));
+            cout << "2)s = " << setprecision(3) << s << endl;
+
+        } else {
+
+            s = cbrt(abs(x)) + pow(x, 3)/y;
+            cout << "3)s = " << setprecision(3) << s << endl;
+
+        }
+
+    } else if (n == 6) {
+
+        cout << "Пример #6." << endl;
+
+        cout << "Введите x: ";
+        cin >> x;
+        cout << "Введите y: ";
+        cin >> y;
+
+        if (0.5 < x*y < 10) {
+
+            s = exp(x-abs(y));
+            cout << "1)s = " << setprecision(3) << s << endl;
+
+        } else if (0.1 < x*y < 0.5) {
+
+            s = cbrt(abs(x+y));
+            cout << "2)s = " << setprecision(3) << s << endl;
+
+        } else {
+
+            s = 2 * pow(x, 2);
+            cout << "3)s = " << setprecision(3) << s << endl;
+
+        }
+
+    } else if (n == 7) {
+
+        cout << "Пример #7." << endl;
+
+        cout << "Введите x: ";
+        cin >> x;
+        cout << "Введите b: ";
+        cin >> b;
+
+        if (1 < x*b < 10) {
+
+            s = exp(-x);
+            cout << "1)s = " << setprecision(3) << s << endl;
+
+        } else if (12 < x*b < 40) {
+
+            s = cbrt(abs(x + 4*y));
+            cout << "2)s = " << setprecision(3) << s << endl;
+
+        } else {
+
+            s = y * pow(x, 2);
+            cout << "3)s = " << setprecision(3) << s << endl;
+
+        }
+
+    } else if (n <= 0 || n > 7) {
+
+        cout << "Извините, но такого примера не существует. Попробуйте перезапустить программу и выбрать другой пример!";
+
+    }
+
+    return 0;
 
 }
-}
-
-if (n == 2) {
-  cout << "Пример #2" << endl;
-
-  cout << "Введите x: ";
-  cin >> x;
-  cout << "Введите y: ";
-  cin >> y;
-
-if (x/y > 0) {
-  s = log10(x) + pow(double(abs(y)),1/3);
-  cout << "1)s = " << setprecision(3) << s << endl;
-
-} else if (x/y < 0) {
-  s = log10(x/y) * pow((x+y),3);
-  cout << "2)s = " << setprecision(3) << s << endl;
-
-} else {
-  s = pow((pow(x),2 * y),3);
-  cout << "3)s = " << setprecision(3) << s << endl;
-
-}
-}
-}
-return 0;
